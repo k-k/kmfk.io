3e154f01acff7caa2f609ba9cfaa17184b0fbd2c
Keith Kirk <kmfk00@gmail.com>
2014-08-18 11:00:53 -0700

    fun emails

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,10 +8,34 @@ class IndexController extends Controller
     {
         $this->tag->setTitle("Keith Kirk // KMFK");
 
+        $emails = [
+            'hello',
+            'hi',
+            'greetings',
+            'salutations',
+            'hola',
+            'i-dont-bite',
+            'email-me-already'
+        ];
+
+        $counter = 0;
+        if ($this->session->has('email-counter')) {
+            $counter = $this->session->get('email-counter');
+        }
+
+        if ($counter < sizeof($emails)) {
+            $this->session->set('email-counter', $counter + 1);
+        } else {
+            $counter = 0;
+            $this->session->set('email-counter', $counter + 1);
+        }
+
         $this->assets
             ->addCss('css/index-temporary.css')
+            ->addCss('css/animate.css')
             ->addCss('//fonts.googleapis.com/css?family=Raleway:700', false);
 
+        $this->view->setVars(['email' => $emails[$counter]]);
     }
 
 }
--- app/views/index/index.volt
+++ app/views/index/index.volt
@@ -1,8 +1,7 @@
 <div id="content">
     <h1 class="hello">Hello, I'm Keith Kirk.</h1>
     <h3 class="title">A full stack Software Engineer</h3>
-    <img class="email-icon" src="img/email-32.png" />
-    <a class="email" href="mailto:hello@kmfk.io">hello@kmfk.io</a>
+    <a class="email" href="mailto:{{ email }}@kmfk.io"><span class="animated pulse">{{ email }}</span>@kmfk.io</a>
     <br />
     <div class="icons">
         <a href="https://github.com/kmfk" target="_blank"><img src="img/github-48.png" /></a>
@@ -10,6 +9,4 @@
         <a href="http://www.linkedin.com/pub/keith-kirk/a1/a10/71a" target="_blank"><img src="img/linkedin-48.png" /></a>
         <a href="https://twitter.com/kmfk" target="_blank"><img src="img/twitter-48.png" /></a>
     </div>
-</div>
-<div id="projects">
 </div>
